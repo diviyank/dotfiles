f5a9da740022296dab4082939955c3c5f3521f1b
Diviyan <diviyan@macbook.local>
2025-01-31 11:24:13 +0100

    [ADAPT]

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,11 @@
 set -gx EDITOR emacs
 set fish_greeting #"Hello Diviyan ! What are you going to break today ?"
-fzf_configure_bindings --directory=\ce
+# Set up fzf key bindings
+set -U fish_user_paths /opt/homebrew/bin $fish_user_paths
+fzf --fish | source
 set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND "--type=f"
 set -gx FZF_CTRL_T_OPTS "--preview='bat --style=numbers --color=always {}'"
 set -gx FZF_ALT_C_COMMAND $FZF_DEFAULT_COMMAND "--type=d"
-alias py xonsh
 alias enw "emacs -nw"
 alias ga "git add -u"
 alias gc "git commit -m"
@@ -23,10 +24,4 @@ end
 starship init fish | source
 pyenv init - | source
 fish_add_path /home/diviyan/.emacs.d/bin
-source $HOME/.grit/bin/env.fish
-set --export GRIT_INSTALL "$HOME/.grit"
-set --export PATH $GRIT_INSTALL/bin $PATH
-function gritp --description "grit python"
-    command grit apply "language python $argv"
-end
 zoxide init --cmd cd fish | source
