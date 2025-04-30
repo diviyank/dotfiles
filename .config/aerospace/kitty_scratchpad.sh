c3fd89694dd1a49c92f3bfb82968042592c9d815
Diviyan Kalainathan <diviyan@fentech.ai>
2025-04-30 09:11:49 +0200

    [DEV] swipe

--- .config/aerospace/kitty_scratchpad.sh
+++ .config/aerospace/kitty_scratchpad.sh
@@ -2,7 +2,7 @@ focus_app() {
   current_workspace=$(/opt/homebrew/bin/aerospace list-workspaces --focused)
   app_window_id=$(/opt/homebrew/bin/aerospace list-windows --all | rg kitty_scratch | awk -F '|' '{print $1}') 
   /opt/homebrew/bin/aerospace move-node-to-workspace --focus-follows-window --window-id $app_window_id $current_workspace
-  # /opt/homebrew/bin/aerospace focus --window-id $app_window_id
+  /opt/homebrew/bin/aerospace focus --window-id $app_window_id
   # /opt/homebrew/bin/aerospace workspace $current_workspace
   /opt/homebrew/bin/aerospace move-mouse window-lazy-center
 }
@@ -26,7 +26,7 @@ if [ "$(/opt/homebrew/bin/aerospace list-windows --focused | rg kitty_scratch | 
 unfocus_app() {
   current_monitor=$(/opt/homebrew/bin/aerospace list-monitors --focused)
   /opt/homebrew/bin/aerospace move-node-to-workspace 0
-  /opt/homebrew/bin/aerospace move-workspace-to-monitor $current_monitor
+  # /opt/homebrew/bin/aerospace move-workspace-to-monitor --workspace 0 $current_monitor
 }
 
 
